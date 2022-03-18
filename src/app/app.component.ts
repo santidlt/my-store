5e0929ca523b176c1a33c36cbf1890224822e7a6
santi de la torre <santiagodlt01@gmail.com>
2022-03-18 20:31:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'san'
+  age = 19
+  img = 'https://source.unsplash.com/random'
+  buttonDisable = true
+
+  person = {
+    name: 'kk',
+    age: 12,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton(){
+    this.buttonDisable = !this.buttonDisable;
+  }
+
+  alertAtSending(){
+    alert('CAGASTE')
+  }
+
+  ageIncrease(){
+    this.person.age += 1
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement
+    this.person.name = element.value
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
